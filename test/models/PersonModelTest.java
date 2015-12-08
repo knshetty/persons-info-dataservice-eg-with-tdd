9cbf05ab7ad8d9c3438d853a5f0546f90a6c2954
knshetty <knshetty@live.com>
2015-12-08 21:41:18 +0200

    Feature: Add Javadocs to each test method

--- test/models/PersonModelTest.java
+++ test/models/PersonModelTest.java
@@ -13,18 +13,28 @@ public class PersonModelTest {
 	private Person bart;
 	private int bartAge;
 	
+	/**
+	 * This method is a setup hooks for test-cases, which ensures that each 
+	 * test-case is dealing with known values.
+	 */
 	@Before
 	public void setUp() throws Exception {
 		bart = new Person(101,"Bart","Simpsons",1987,"Springfield","USA");
 		bartAge = (currentYear - bart.getBirthYear());
 	}
-
+	
+	/**
+	 * This method tests the correctness of computed age of the person
+	 */
 	@Test
 	public void getAgeReturnItem() {
 		int expectedAge = bartAge;
 		assertEquals(expectedAge, bart.getAge());
 	}
 	
+	/**
+	 * This method tests the expected string serialisation of the Person model
+	 */
 	@Test
 	public void toStringReturnItem() {
 		String expectedToString = "firstName: Bart, lastName: Simpsons, age: " + bartAge; 
