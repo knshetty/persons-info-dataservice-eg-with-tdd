0e87a90326cdc7cce8e6887530ef1c298b3bf4ff
knshetty <knshetty@live.com>
2015-12-08 03:09:33 +0200

    Refactor: Amend Javadocs descriptions related to class & method

--- src/datasources/IDataSource.java
+++ src/datasources/IDataSource.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
 import models.Person;
 
 /**
- * This interface specifies a generic data-provider to obtain persons dataset 
+ * This interface specifies a generic data-provider to extract persons dataset 
  * from various resources.
  * @author Nash
  */
 public interface IDataSource {
 	
 	/**
-	 * This method implements the extraction of persons dataset from resource.
+	 * This method implements the extraction mechanics of persons dataset 
+	 * from a given resource.
 	 * @return dataset of persons
 	 */
 	ArrayList<Person> getPersons();
